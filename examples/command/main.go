d50c4bfaab835f4de0ff8c9f9f78ae9e95a90921
erdaltsksn <erdal@taskesen.me>
2020-07-03 14:02:17 +0300

    docs: (example) Add examples for VersionCmd

---
+++ examples/command/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/erdaltsksn/cui"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(cui.VersionCmd)
+}
