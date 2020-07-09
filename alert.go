cd6e35b3052b001ba6843f2bf7780b3591a3521b
Niko Köser <koeserniko@gmail.com>
2020-07-09 22:39:56 +0200

    refactor: remove redundant empty check
    You don't need to check for an empty array before your 'for-each' loop;
    when the array is empty the loop doesn't have any elements to loop over
    in the first place and basically does nothing.
    So validating the arrays before the loops doesn't change the outcome of
    your functions but improves code simplicity.

--- alert.go
+++ alert.go
@@ -10,10 +10,8 @@ import (
 // Success prints a success message and exit status 0
 func Success(message string, details ...string) {
 	color.Success.Println("√", message)
-	if len(details) > 0 {
-		for _, detail := range details {
-			color.Primary.Println("-", detail)
-		}
+	for _, detail := range details {
+		color.Primary.Println("-", detail)
 	}
 	os.Exit(0)
 }
@@ -21,10 +19,8 @@ func Success(message string, details ...string) {
 // Error prints a success message and exit status 1
 func Error(message string, err ...error) {
 	color.Danger.Println("X", message)
-	if len(err) > 0 {
-		for _, e := range err {
-			fmt.Println(" ", e.Error())
-		}
+	for _, e := range err {
+		fmt.Println(" ", e.Error())
 	}
 	os.Exit(1)
 }
@@ -32,10 +28,8 @@ func Error(message string, err ...error) {
 // Warning prints a warning message and exit status 1
 func Warning(message string, details ...string) {
 	color.Warn.Println("!", message)
-	if len(details) > 0 {
-		for _, detail := range details {
-			fmt.Println(" ", detail)
-		}
+	for _, detail := range details {
+		fmt.Println(" ", detail)
 	}
 	os.Exit(1)
 }
