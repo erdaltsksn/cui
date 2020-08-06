a0a6769b64d041be315ea2d825ab7601c2fd8bd6
erdaltsksn <erdal@taskesen.me>
2020-08-06 07:57:57 +0300

    tidy: (command) Use Imperative mood for help

--- command.go
+++ command.go
@@ -9,8 +9,8 @@ var appVersion = "unknown"
 // VersionCmd represents the version command.
 var VersionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Prints the app's version",
-	Long: `Prints the app's version. This app uses semver and the version value
+	Short: "Print the app's version",
+	Long: `Print the app's version. This app uses semver and the version value
 is automatically generated while building.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		Info(appVersion)
